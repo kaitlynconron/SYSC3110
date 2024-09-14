90aaa0a8a75ff372f3428701a5b13ea3bd180388
Kaitlyn Conron <kaitlynconron@cmail.carleton.ca>
2024-09-14 17:48:30 -0400

    doing step 8

---
+++ src/AddressBook.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private final ArrayList<BuddyInfo> book;
+
+    public AddressBook() {
+        this.book = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo pal) {
+        this.book.add(pal);
+    }
+
+    public void removeBuddy(BuddyInfo pal) {
+        this.book.remove(pal);
+    }
+
+    public static void main(String[] args) {
+        /*
+        AddressBook ab = new AddressBook();
+        BuddyInfo bud = new BuddyInfo("kait", "Home", 7007);
+        BuddyInfo guy = new BuddyInfo("link", "his house", 40009);
+        ab.addBuddy(bud);
+        ab.addBuddy(guy);
+        System.out.println(ab.book.get(0).getName());
+        ab.removeBuddy(bud);
+        System.out.println(ab.book.get(0).getName());
+        */
+        System.out.println("Address Book");
+    }
+}
